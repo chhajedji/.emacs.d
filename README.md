2aa7025b154035b7563aeec55fed7d493687fff8
Chinmay Chhajed <chhajedchinmay@gmail.com>
2023-04-20 14:37:40 -0400

    Initial Commit for Emacs 2.0
    * Removed all files and started fresh. Old file can be found here:
      https://github.com/chhajedji/.emacs.d/tree/ad18b5d2909a4a3726fde85a45f5268421632aa0.
    * Add README with basic rant and some instructions
    * init.el has commands to setup MELPA.
    * Added a config.org file which shall hold all the cofig files.
    * init.el also has the `org-babel-load-file` command which will load the
      configuration from config.org

--- README.md
+++
@@ -1,3 +0,0 @@
-## chhajedji's build on vanilla emacs.
-
-[init.el](./init.el) is just for initializing and doing the basic setup. Check [config.org](./config.org) for detailed build. `init.el` mainly sources `config.org`.
--- init.el
+++ init.el
@@ -1,91 +1,9 @@
 (require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;; Any add to list for package-archives (to add marmalade or melpa) goes here
+(add-to-list 'package-archives 
+    '("MELPA" .
+      "http://melpa.org/packages/"))
 (package-initialize)
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-
-;;;;
-
-;; Load config.org file.
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
- '(custom-enabled-themes nil)
- '(custom-safe-themes
-   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
- '(evil-undo-system nil)
- '(fci-rule-color "#2e2e2e")
- '(fringe-mode 10 nil (fringe))
- '(global-hl-line-mode nil)
- '(helm-completion-style 'emacs)
- '(hl-todo-keyword-faces
-   '(("TODO" . "#dc752f")
-     ("NEXT" . "#dc752f")
-     ("THEM" . "#2d9574")
-     ("PROG" . "#4f97d7")
-     ("OKAY" . "#4f97d7")
-     ("DONT" . "#f2241f")
-     ("FAIL" . "#f2241f")
-     ("DONE" . "#86dc2f")
-     ("NOTE" . "#b1951d")
-     ("KLUDGE" . "#b1951d")
-     ("HACK" . "#b1951d")
-     ("TEMP" . "#b1951d")
-     ("FIXME" . "#dc752f")
-     ("XXX+" . "#dc752f")
-     ("\\?\\?\\?+" . "#dc752f")))
- '(idle-highlight-exceptions '(""))
- '(linum-format " %6d ")
- '(main-line-color1 "#222232")
- '(main-line-color2 "#333343")
- '(package-selected-packages
-   '(lsp-sourcekit php-mode lua-mode afternoon-theme projectile helm smex ido-vertical-mode company-irony irony company idle-highlight-mode idle-highlight-symbol highlight-symbol spaceline yaml-mode xcscope undo-fu markdown-mode popup-kill-ring diminish rainbow-delimiters switch-window rainbow-mode magit evil org-bullets beacon which-key use-package lua-mode))
- '(pdf-view-midnight-colors '("#b2b2b2" . "#292b2e"))
- '(powerline-color1 "#222232")
- '(powerline-color2 "#333343")
- '(vc-annotate-background "#3b3b3b")
- '(vc-annotate-color-map
-   '((20 . "#dd5542")
-     (40 . "#CC5542")
-     (60 . "#fb8512")
-     (80 . "#baba36")
-     (100 . "#bdbc61")
-     (120 . "#7d7c61")
-     (140 . "#6abd50")
-     (160 . "#6aaf50")
-     (180 . "#6aa350")
-     (200 . "#6a9550")
-     (220 . "#6a8550")
-     (240 . "#6a7550")
-     (260 . "#9b55c3")
-     (280 . "#6CA0A3")
-     (300 . "#528fd1")
-     (320 . "#5180b3")
-     (340 . "#6380b3")
-     (360 . "#DC8CC3")))
- '(vc-annotate-very-old-color "#DC8CC3")
- '(window-divider-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Load the settings from config.org file.
+(org-babel-load-file "~/.emacs.d/config.org")

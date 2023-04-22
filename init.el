c7201ac0a35ae09ccf105753ec59db037672ed17
Chinmay Chhajed <chhajedchinmay@gmail.com>
2023-04-22 16:57:59 -0400

    Added init for ORG mode and which-keys and default stuff in init.el

--- init.el
+++ init.el
@@ -7,3 +7,16 @@
 
 ;; Load the settings from config.org file.
 (org-babel-load-file "~/.emacs.d/config.org")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(which-key)))
+
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+(custom-set-faces)

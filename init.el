da2169be4fc216a98fae32f3ed9bc2c51c0d21d2
Chinmay Chhajed <chhajedchinmay@gmail.com>
2023-04-23 15:18:43 -0400

    Some more packages and general settings.
    * Evil
    * Relative line number.

--- init.el
+++ init.el
@@ -13,10 +13,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(which-key)))
+ '(package-selected-packages '(evil which-key)))
 
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-(custom-set-faces)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

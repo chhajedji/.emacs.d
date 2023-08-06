5974d6b010adade90f7f4ce9f5266d66b41bea9f
Chinmay Chhajed <chhajedchinmay@gmail.com>
2023-08-06 19:23:01 -0400

    Minor updates.
    * Update dictionary from nothing (nil) to something (not nil) to use.
    * Modified idle highlight time to 2 secs from 0.35 secs.
    * Set scratch buffer message and to initialize it in text mode, rather
      than usual elisp mode.
    * Truly relative line numbering across all buffers (refer
      https://emacs.stackexchange.com/a/41764/29032).

--- init.el
+++ init.el
@@ -16,8 +16,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(global-hl-line-mode nil)
- '(ispell-dictionary nil)
  '(package-selected-packages
    '(helm xcscope magit beacon org-bullets idle-highlight-mode solarized-theme evil which-key)))
 
